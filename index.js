d329c64cf081f0b7e288dfde87539fe02453ab93
Darius <dariusmmcclendon@gmail.com>
2022-02-17 15:18:17 -0500

    Added places controller.

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ require('dotenv').config()
 let express = require('express')
 let app = express()
 
+app.use('/places',require('./controllers/places.js'))
+//default route
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-
+//404 route
 app.get('*',(req, res)=>{
     res.status(404).send(`<h1>404 Page<h1>`)
 })
