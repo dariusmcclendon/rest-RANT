878e93749d8c597009b70de69234e73f1e20e891
Darius <dariusmmcclendon@gmail.com>
2022-02-20 10:30:41 -0500

    Added default view for layout

---
+++ views/default.jsx
@@ -0,0 +1,17 @@
+let React = require('react')
+
+function Def (html) {
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                (html.children)
+            </body>
+        </html>
+    )
+
+}
+
+module.exports = Def
