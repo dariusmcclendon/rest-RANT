2739bdb441e66f2160a0f63453a05272d43cc9b6
Darius <dariusmmcclendon@gmail.com>
2022-02-20 10:23:47 -0500

    Updated README file to include routing table.

--- index.js
+++ index.js
@@ -3,13 +3,61 @@ let express = require('express')
 let app = express()
 
 app.use('/places',require('./controllers/places.js'))
+
+
 //default route
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
+
 //404 route
 app.get('*',(req, res)=>{
     res.status(404).send(`<h1>404 Page<h1>`)
+})
+
+//places index page
+app.get('/places', (req,res) => {
+    res.send('Places index page')
+})
+
+//create new place
+app.post('/places', (req, res) =>{
+    res.send('Create a new place')
+})
+
+//form page for creating a new place
+app.get('/places/new', (req, res) => {
+    res.send('Form page, plew places')
+})
+
+//details about a particular place
+app.get('/places/:id', (req, res) => {
+    res.send('place info')
+})
+
+//update a particular place
+app.put('/places/:id', (req, res) => {
+    res.send('update')
+})
+
+//form to edit a place
+app.get('/places/:id/edit', (req, res) => {
+    res.send('form page, update place')
+})
+
+//delete a place
+app.delete('/places/:id', (req, res) => {
+    res.send('delete place')
+})
+
+//create a rant about a particular place
+app.post('/places/:id/rant', (req, res) => {
+    res.send('create rant')
+})
+
+//delete a rant
+app.delete('/places/:id/rant/rantId', (req, res) => {
+    res.send('delete rant')
 })
 
 app.listen(process.env.PORT)
