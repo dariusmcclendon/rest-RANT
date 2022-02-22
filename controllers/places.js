fbf7f634183827d7e48a930e336cf96c065d8e76
Darius <dariusmmcclendon@gmail.com>
2022-02-22 16:34:50 -0500

    Confirmed ability to crate new place. Added default values for img, city and state if none are provided.

--- controllers/places.js
+++ controllers/places.js
@@ -1,20 +1,26 @@
 let router = require('express').Router()
-
+let places = require('../models/places.js')
 router.get('/',(req,res)=>{
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
-      }]
+    
     res.render('places/index', {places})
+})
+
+router.post('/', (req,res)=>{
+  // debug
+  // console.log(req.body)
+  if (!req.body.pic){
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city){
+    // Default city if one is not provided
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state){
+    req.body.state = 'USA'
+  }
+  places.push(req.body)  
+  res.redirect('/places')
 })
 
 router.get('/new',(req,res)=>{
