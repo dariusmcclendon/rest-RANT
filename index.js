a04f85e24ed5706c7733bb31a6ff2dc2dabcf830
Darius <dariusmmcclendon@gmail.com>
2022-03-08 10:46:21 -0500

    Added seeder script. Activity instructions...wrong?

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //Modules and Globals
-require('dotenv').config()
 let express = require('express')
 let app = express()
 let methodOverride = require('method-override')
@@ -27,36 +26,6 @@ app.get('*',(req, res)=>{
 app.get('/places', (req,res) => {
     res.render('index')
 })
-
-//create new place
-// app.post('/places', (req, res) =>{
-//     res.send('Create a new place')
-// })
-
-// //form page for creating a new place
-// app.get('/places/new', (req, res) => {
-//     res.send('Form page, plew places')
-// })
-
-// //details about a particular place
-// app.get('/places/:id', (req, res) => {
-//     res.send('place info')
-// })
-
-// //update a particular place
-// app.put('/places/:id', (req, res) => {
-//     res.send('update')
-// })
-
-// //form to edit a place
-// app.get('/places/:id/edit', (req, res) => {
-//     res.send('form page, update place')
-// })
-
-// //delete a place
-// app.delete('/places/:id', (req, res) => {
-//     res.send('delete place')
-// })
 
 //create a rant about a particular place
 app.post('/places/:id/rant', (req, res) => {
